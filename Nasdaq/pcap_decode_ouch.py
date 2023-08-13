01d0d2aded6e161617f5840565675cff353431ce
grmclub <grmclub19@gmail.com>
2023-08-13 15:37:03 +0900

    Update tabs to spaces

--- Nasdaq/pcap_decode_ouch.py
+++ Nasdaq/pcap_decode_ouch.py
@@ -37,131 +37,131 @@ data_list = []
 
 
 class jnx_ouch:
-	def __init__(self,buf):
-		self.header = struct.unpack('>Hcc', buf[0:4])
-		self.size = int(self.header[0])
-		self.soup_pkt_type = self.header[1]
-		self.ouch_msg_type = self.header[2]
-		self.ouch_buf = buf[4:]
-		self.buf_len = len(buf)
-		#print("Buf_len=%d" % (self.buf_len))
-		#print(data_len=%d,size=%d" % (len(buf),self.size))
-		
-	def print_headers(self):
-		print(self.size,self.soup_pkt_type,self.ouch_msg_type)
-		
-	def decode(self):
-		ext = ""
-		if self.buf_len >=4:
-			ouch_buf = self.ouch_buf[0:self.size]
-			#self.print_headers()
-			if self.ouch_msg_type == 'O':
-				ext = self.output_msg(ouch_buf, '>I10scII4sIIIccIcc')
-			elif self.ouch_msg_type == 'U' and self.soup_pkt_type == 'U':
-				ext = self.output_msg(ouch_buf, '>IIIIIcI')
-			elif self.ouch_msg_type == 'X':
-				ext = self.output_msg(ouch_buf, '>II')
-			elif self.ouch_msg_type == 'A':
-				ext = self.output_msg(ouch_buf[0:64], '>QI10scII4sIIIccQIccc')
-			elif self.ouch_msg_type == 'C':
-				ext = self.output_msg(ouch_buf, '>QIIc')
-			elif self.ouch_msg_type == 'U'  and self.soup_pkt_type == 'S':
-				ext = self.output_msg(ouch_buf[0:51], '>QIcII4sIIcQIcI')
-			elif self.ouch_msg_type == 'D':
-				ext = self.output_msg(ouch_buf, '>QIIcIIc')
-			elif self.ouch_msg_type == 'E':
-				ext = self.output_msg(ouch_buf[0:29], '>QIIIcQ')
-			elif self.ouch_msg_type == 'J':
-				ext = self.output_msg(ouch_buf, '>QIc')
-			elif self.ouch_msg_type == 'S':
-				ext = self.output_msg(ouch_buf, '>Qc')
-		return ext	
-				
- 	def output_msg(self,buf,unpack_str):
-		global data_list
-		del data_list
-		data_list = struct.unpack(unpack_str, buf)
-		return("%d,%s,%s,%s"% (self.header[0],self.header[1],self.header[2], ",".join(str(x) for x in data_list)))
-	
-	
+    def __init__(self,buf):
+        self.header = struct.unpack('>Hcc', buf[0:4])
+        self.size = int(self.header[0])
+        self.soup_pkt_type = self.header[1]
+        self.ouch_msg_type = self.header[2]
+        self.ouch_buf = buf[4:]
+        self.buf_len = len(buf)
+        #print("Buf_len=%d" % (self.buf_len))
+        #print(data_len=%d,size=%d" % (len(buf),self.size))
+        
+    def print_headers(self):
+        print(self.size,self.soup_pkt_type,self.ouch_msg_type)
+        
+    def decode(self):
+        ext = ""
+        if self.buf_len >=4:
+            ouch_buf = self.ouch_buf[0:self.size]
+            #self.print_headers()
+            if self.ouch_msg_type == 'O':
+                ext = self.output_msg(ouch_buf, '>I10scII4sIIIccIcc')
+            elif self.ouch_msg_type == 'U' and self.soup_pkt_type == 'U':
+                ext = self.output_msg(ouch_buf, '>IIIIIcI')
+            elif self.ouch_msg_type == 'X':
+                ext = self.output_msg(ouch_buf, '>II')
+            elif self.ouch_msg_type == 'A':
+                ext = self.output_msg(ouch_buf[0:64], '>QI10scII4sIIIccQIccc')
+            elif self.ouch_msg_type == 'C':
+                ext = self.output_msg(ouch_buf, '>QIIc')
+            elif self.ouch_msg_type == 'U'  and self.soup_pkt_type == 'S':
+                ext = self.output_msg(ouch_buf[0:51], '>QIcII4sIIcQIcI')
+            elif self.ouch_msg_type == 'D':
+                ext = self.output_msg(ouch_buf, '>QIIcIIc')
+            elif self.ouch_msg_type == 'E':
+                ext = self.output_msg(ouch_buf[0:29], '>QIIIcQ')
+            elif self.ouch_msg_type == 'J':
+                ext = self.output_msg(ouch_buf, '>QIc')
+            elif self.ouch_msg_type == 'S':
+                ext = self.output_msg(ouch_buf, '>Qc')
+        return ext  
+                
+    def output_msg(self,buf,unpack_str):
+        global data_list
+        del data_list
+        data_list = struct.unpack(unpack_str, buf)
+        return("%d,%s,%s,%s"% (self.header[0],self.header[1],self.header[2], ",".join(str(x) for x in data_list)))
+    
+    
 class soupbintcp:
-	def __init__(self,app_handler):
-		self.app_handler = app_handler
-		
-	def decode(self,buf):
-		buf_len = len(buf)
-		ext = ""
-		if buf_len >=3:
-			size,soup_pkt_type = struct.unpack('>Hc', buf[0:3])
-			soup_buf = buf[4:]
-			soup_buf = soup_buf[0:size]
-			#if (buf_len != size +2):
-			#	print("Buf_len=%d" %(buf_len))
-			#self.output_hdr(size,soup_pkt_type)
-			if soup_pkt_type == '+':
-				ext = self.output_hdr(size,soup_pkt_type)
-			elif soup_pkt_type == 'H':
-				ext = self.output_hdr(size,soup_pkt_type)				
-			elif soup_pkt_type == 'R':
-				ext = self.output_hdr(size,soup_pkt_type)
-			elif soup_pkt_type == 'Z':
-				ext = self.output_hdr(size,soup_pkt_type)				
-			elif soup_pkt_type == 'L':
-				ext = self.output_hdr(size,soup_pkt_type)
-				#self.output_soup(size,soup_pkt_type,soup_buf[0:45],'6s10s10s20s')  ##XXX -Fix
-			elif soup_pkt_type == 'A':
-				ext = self.output_hdr(size,soup_pkt_type)
-				#self.output_soup(size,soup_pkt_type,soup_buf,'10s20s')  ##XXX -Fix				
-			elif soup_pkt_type == 'J':
-				ext = self.output_soup(size,soup_pkt_type,soup_buf,'c')
-			elif soup_pkt_type == 'O':
-				ext = self.output_hdr(size,soup_pkt_type)				
-			elif soup_pkt_type == 'S':
-				handler = jnx_ouch(buf)
-				ext = handler.decode()
-			elif soup_pkt_type == 'U':
-				handler = jnx_ouch(buf)
-				ext = handler.decode()
-		return ext
-			
+    def __init__(self,app_handler):
+        self.app_handler = app_handler
+        
+    def decode(self,buf):
+        buf_len = len(buf)
+        ext = ""
+        if buf_len >=3:
+            size,soup_pkt_type = struct.unpack('>Hc', buf[0:3])
+            soup_buf = buf[4:]
+            soup_buf = soup_buf[0:size]
+            #if (buf_len != size +2):
+            #   print("Buf_len=%d" %(buf_len))
+            #self.output_hdr(size,soup_pkt_type)
+            if soup_pkt_type == '+':
+                ext = self.output_hdr(size,soup_pkt_type)
+            elif soup_pkt_type == 'H':
+                ext = self.output_hdr(size,soup_pkt_type)               
+            elif soup_pkt_type == 'R':
+                ext = self.output_hdr(size,soup_pkt_type)
+            elif soup_pkt_type == 'Z':
+                ext = self.output_hdr(size,soup_pkt_type)               
+            elif soup_pkt_type == 'L':
+                ext = self.output_hdr(size,soup_pkt_type)
+                #self.output_soup(size,soup_pkt_type,soup_buf[0:45],'6s10s10s20s')  ##XXX -Fix
+            elif soup_pkt_type == 'A':
+                ext = self.output_hdr(size,soup_pkt_type)
+                #self.output_soup(size,soup_pkt_type,soup_buf,'10s20s')  ##XXX -Fix             
+            elif soup_pkt_type == 'J':
+                ext = self.output_soup(size,soup_pkt_type,soup_buf,'c')
+            elif soup_pkt_type == 'O':
+                ext = self.output_hdr(size,soup_pkt_type)               
+            elif soup_pkt_type == 'S':
+                handler = jnx_ouch(buf)
+                ext = handler.decode()
+            elif soup_pkt_type == 'U':
+                handler = jnx_ouch(buf)
+                ext = handler.decode()
+        return ext
+            
 
-	def output_hdr(self,size,pkt_type):
-		return ("%d,%s" % (size,pkt_type))
-		
-	def output_soup(self,size,pkt_type,buf,unpack_str):
-		data_list = struct.unpack(unpack_str,buf)
-		return("%d,%s,%s"% (size,pkt_type, ",".join(str(x) for x in data_list)))		
-			
+    def output_hdr(self,size,pkt_type):
+        return ("%d,%s" % (size,pkt_type))
+        
+    def output_soup(self,size,pkt_type,buf,unpack_str):
+        data_list = struct.unpack(unpack_str,buf)
+        return("%d,%s,%s"% (size,pkt_type, ",".join(str(x) for x in data_list)))        
+            
 
 def extract_from_pcap_file(pcap_file,out_file):
-	cmd = """tshark -r %s \
-	-T fields \
-	-Eseparator=, \
-	-Eoccurrance=1 \
-	-e frame.time_epoch \
-	-e data > %s """ % (pcap_file,out_file)
-	result,e,r = execCmd(cmd)
+    cmd = """tshark -r %s \
+    -T fields \
+    -Eseparator=, \
+    -Eoccurrance=1 \
+    -e frame.time_epoch \
+    -e data > %s """ % (pcap_file,out_file)
+    result,e,r = execCmd(cmd)
 
 
-		
+        
 def process_file(data_file):
-	app_handler = "jnx_ouch"
-	soup_handler = soupbintcp(app_handler)
+    app_handler = "jnx_ouch"
+    soup_handler = soupbintcp(app_handler)
     with open(data_file, "r") as ifile:
-		for line in ifile:
-			row = line.split(',')
-			row_len = len(row)
-			ts = "x"
-			if row_len == 2:
-				ts = row[0]
-				line = row[1].strip()
-			else:
-				line = row[0].strip()
-				
-			if len(line) > 1:
-				buf = bytearray.fromhex(line.strip())
-				ext = soup_handler.decode(buf)
-				print("%s,%s" % (ts,ext))
+        for line in ifile:
+            row = line.split(',')
+            row_len = len(row)
+            ts = "x"
+            if row_len == 2:
+                ts = row[0]
+                line = row[1].strip()
+            else:
+                line = row[0].strip()
+                
+            if len(line) > 1:
+                buf = bytearray.fromhex(line.strip())
+                ext = soup_handler.decode(buf)
+                print("%s,%s" % (ts,ext))
 
 
 def main():
@@ -173,8 +173,8 @@ def main():
 
     try:
         DATA_FILE = ""
-		PCAP_FILE = ""
-		OUT_FILE  = "./ext.bin"
+        PCAP_FILE = ""
+        OUT_FILE  = "./ext.bin"
         for o,a in opts:
             if o == "-f":
                 DATA_FILE = a
@@ -184,9 +184,9 @@ def main():
                 printHelp()
                 sys.exit(0)
 
-		if PCAP_FILE != "":
-			extract_from_pcap_file(PCAP_FILE,OUT_FILE)
-			DATA_FILE = OUT_FILE
+        if PCAP_FILE != "":
+            extract_from_pcap_file(PCAP_FILE,OUT_FILE)
+            DATA_FILE = OUT_FILE
         process_file(DATA_FILE)
 
     except Exception, err:
