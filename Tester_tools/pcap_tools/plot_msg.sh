1840676f3c89f5b83373f892943ab85df47fbe83
grmclub190 <grmclub19@gmail.com>
2023-08-20 20:05:07 +0900

    Update tester tools

---
+++ Tester_tools/pcap_tools/plot_msg.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/gnuplot
+reset
+jst = 32400      #JST time adjustment
+set title 'XXX Messages'
+set ylabel 'messages'
+set grid
+set xdata time
+set timefmt '%s'
+set format x '%H:%M:%S'
+set format y '%10.0f'
+#set terminal svg size 2311,1506
+#set terminal png size 2311,1506
+set terminal png size 1024,768
+#set terminal svg size 1024,768
+
+#set output "out.svg"
+set output "out.png"
+pl "gplot2.csv" u ($2+jst):1 w impulses t 'msg/sec'
+set terminal X11
+
+
---
+++ Tester_tools/sftp_tester.sh
@@ -0,0 +1,29 @@
+IDENTITYFILE=""
+FTP_USER=""
+SERVER=""
+DIR_LIST=""
+LOGFILE=""
+
+sftp -o Port=22 -o ${IDENTITYFILE} ${FTP_USER}@${SERVER} << EOF > ${DIR_LIST}
+	cd $FTP_DIR
+	dir
+	exit
+EOF
+
+	RET_CODE=$?
+	if [ $RET_CODE -ne 0 ]; then
+		echo -e "Failed to get the ftp dir list ret_code :$RET_CODE"
+		exit 5
+	fi
+
+	sftp -o Port=22 -o ${IDENTITYFILE} ${FTP_USER}@${SERVER} << EOF > ${LOGFILE}
+	cd $FTP_DIR
+	get $READ_FILE $OUTFILE
+	exit
+EOF
+
+	RET_CODE=$?
+	if [ $RET_CODE -ne 0 ]; then
+		echo -e "Failed to FTP $READ_FILE from $SERVER ret_code :$RET_CODE"
+		exit 5
+	fi
