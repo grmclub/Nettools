8661621a31e7b43fad24c3f900a11faf84d9374b
grmclub <grmclub19@gmail.com>
2023-08-22 20:53:51 +0900

    Update calc_fix_delay.py

--- Tester_tools/misc_tools/calc_fix_delay.py
+++ Tester_tools/misc_tools/calc_fix_delay.py
@@ -12,12 +12,12 @@ def printHelp():
 
     OPTIONS:
          -f  input file
-		 -l compare log_ts and transact_ts
+         -l compare log_ts and transact_ts
          -h  Display this help message.
-		 
-	Extract tags : 52,60.11
+
+    Extract tags : 52,60.11
     #./calc_fix_delay.py -f <extracted file> |sort-t',' -k6 -nr |cut -d',' -f1,6
-	
+
     """ % os.path.basename(__file__)
 
 
@@ -26,56 +26,56 @@ def printHelp():
 
 def process_tags(data_file):
     with open(data_file, "r") as ifile:
-		reader = csv.reader(ifile)
-		today  = datetime.date.today().strftime("%Y%m%d")
-		offset = datetime.datetime.now() - datetime.datetime.utcnow()
-		
-		for row in reader:
-			# exclude header row.
-			if row[0] == 52:
-				continue
-			else:
-				sending_t  = row[0] 
-				transact_t = row[1]
-				clordid    = row[2]
-				
-			if sending_t != '' and transact_t != '':
-				st = datetime.datetime.strptime(sending_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
-				tt = datetime.datetime.strptime(transact_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
-				delay = abs(float(tt) - float(st))
-				if transact_t !=0:
-					print("%s,%s,%s,%f" % (sending_t,ransact_t,clordid,delay))
-			
+        reader = csv.reader(ifile)
+        today  = datetime.date.today().strftime("%Y%m%d")
+        offset = datetime.datetime.now() - datetime.datetime.utcnow()
+
+        for row in reader:
+            # exclude header row.
+            if row[0] == 52:
+                continue
+            else:
+                sending_t  = row[0]
+                transact_t = row[1]
+                clordid    = row[2]
+
+            if sending_t != '' and transact_t != '':
+                st = datetime.datetime.strptime(sending_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
+                tt = datetime.datetime.strptime(transact_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
+                delay = abs(float(tt) - float(st))
+                if transact_t !=0:
+                    print("%s,%s,%s,%f" % (sending_t,ransact_t,clordid,delay))
+
 def process_logtime(data_file):
     with open(data_file, "r") as ifile:
-		reader = csv.reader(ifile)
-		today  = datetime.date.today().strftime("%Y%m%d")
-		offset = datetime.datetime.now() - datetime.datetime.utcnow()
-		
-		for row in reader:
-			log_t = row[0].split("-")[1]
-			lt = today + '-' + log_t.strip()
-			lt_calc = datetime.datetime.strptime(lt, "%Y%m%d-%H:%M:%S.%f")
-			lt_calc = lt_calc - offset
-			lt_calc = lt_calc.strftime("%s.%f")
-			
-			#sending_t  = row[0] 
-			transact_t = row[1]
-			transact_t = transact_t[0:24]
-			clordid    = row[2]
-				
-			if "-" in transact_time:
-				#st = datetime.datetime.strptime(sending_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
-				tt = datetime.datetime.strptime(transact_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
-			else:
-				transact_t = 0
-				tt = str(0) 
-				
-			delay = abs(float(lt_calc) - float(tt))
-			if transact_t !=0:
-				#print("%s,%s,%s,%f" % (sending_t,ransact_t,clordid,delay))
-				print("%s,%s,%s,%f" % (lt,ransact_t,clordid,delay))
-			
+        reader = csv.reader(ifile)
+        today  = datetime.date.today().strftime("%Y%m%d")
+        offset = datetime.datetime.now() - datetime.datetime.utcnow()
+
+        for row in reader:
+            log_t = row[0].split("-")[1]
+            lt = today + '-' + log_t.strip()
+            lt_calc = datetime.datetime.strptime(lt, "%Y%m%d-%H:%M:%S.%f")
+            lt_calc = lt_calc - offset
+            lt_calc = lt_calc.strftime("%s.%f")
+
+            #sending_t  = row[0]
+            transact_t = row[1]
+            transact_t = transact_t[0:24]
+            clordid    = row[2]
+
+            if "-" in transact_time:
+                #st = datetime.datetime.strptime(sending_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
+                tt = datetime.datetime.strptime(transact_t, "%Y%m%d-%H:%M:%S.%f").strftime("%s.%f")
+            else:
+                transact_t = 0
+                tt = str(0)
+
+            delay = abs(float(lt_calc) - float(tt))
+            if transact_t !=0:
+                #print("%s,%s,%s,%f" % (sending_t,ransact_t,clordid,delay))
+                print("%s,%s,%s,%f" % (lt,ransact_t,clordid,delay))
+
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "f:lh")
@@ -85,20 +85,20 @@ def main():
 
     try:
         DATA_FILE = ""
-		LOG_FLAG = False
+        LOG_FLAG = False
         for o,a in opts:
             if o == "-f":
                 DATA_FILE = a
             if o == "-l":
-                LOG_FLAG = True				
+                LOG_FLAG = True
             if o == "-h":
                 printHelp()
                 sys.exit(0)
 
-		if LOG_FLAG:
-			process_logtime(DATA_FILE)
-		else:
-			process_tags(DATA_FILE)
+        if LOG_FLAG:
+            process_logtime(DATA_FILE)
+        else:
+            process_tags(DATA_FILE)
 
     except Exception, err:
         traceback.print_exc(file=sys.stderr)
