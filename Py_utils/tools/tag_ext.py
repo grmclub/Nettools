1d7b339a552e931c72f11b683b5fd00423829810
grmclub <grmclub19@gmail.com>
2023-09-06 21:37:19 +0900

    Update for python3

--- Py_utils/tools/tag_ext.py
+++ Py_utils/tools/tag_ext.py
@@ -1,31 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os, sys, getopt
 import errno, traceback
 
 def printHelp():
-    print """Usage: %s -f <filename>
+    print ("""Usage: %s -f <filename>
 
     OPTIONS:
          -d delimeter
          -l extract log timestamp
          -t tag list (tag1,tag2)
          -h Display this help message.
-    """ % os.path.basename(__file__)
+    """ % os.path.basename(__file__))
     sys.exit(0)
 
 def format_str(str):
     result = str[:-1] if str[-1] == "," else str
     return result
 
-def process_file(data_file,delm_tag,tag_list,log_ts_flag):
-    print tag_list
+def process_file(data_file,delm,tag_list,log_ts_flag):
+    print(tag_list)
     tag_list = tag_list.split(",")
     tl_size = len(tag_list)
     row_dict= {}
     log_ts = ""
     for line in data_file:
         line = line.strip().split(delm)
+        #print(line)
+        #print(tag_list)
         row_dict.clear()
         for item in line:
             item = item.split('=')
@@ -44,13 +46,13 @@ def process_file(data_file,delm_tag,tag_list,log_ts_flag):
             if log_ts[0:2].isdigit():
                 print("%s,%s" % (log_ts,format_str(str)))
         else:
-            print format_str(str)
+            print(format_str(str))
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hd:f:lt")
+        opts, args = getopt.getopt(sys.argv[1:], "hd:f:lt:")
     except getopt.GetoptError as e:
-        print e
+        print(e)
         sys.exit()
 
     try:
@@ -74,7 +76,7 @@ def main():
 
         process_file(DATA_FILE,DELM,TAG_LIST,LOG_TS_FLAG)
 
-    except Exception, err:
+    except Exception as err:
         traceback.print_exc(file=sys.stderr)
         sys.stderr.flush()
         print("Error: %s\n" %str(err))
