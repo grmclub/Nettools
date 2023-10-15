4c57e0b70acfcd2dd23dca7751ee164747d69d45
grmclub <grmclub19@gmail.com>
2023-10-15 23:26:53 +0900

    Update simple_template.py

--- Script_Templates/simple_template.py
+++ Script_Templates/simple_template.py
@@ -6,36 +6,49 @@ import csv
 from decimal import Decimal
 
 
+class Item:
+    self.m_code
+    self.m_price
+    self.m_desc
+
+    def __init__(self):
+        self.m_price = 0
+        self.m_name  = ""
+
+    def __init__(self, price, name):
+        self.m_price = price
+        self.m_name  = name
+
 def printHelp():
-    print """Usage: %s -f <filename>
+    print ("""Usage: %s -f <filename>
 
     OPTIONS:
          -f  input file
          -h  Display this help message.
-    """ % os.path.basename(__file__)
+    """ % os.path.basename(__file__))
 
 
 def process_file(data_file):
     with open(data_file, "r") as ifile:
-		reader = csv.reader(ifile)
-		rownum = 0
-		for row in reader:
-			# Save header row.
-			if rownum == 0:
-				header = row
-			else:
-				colnum = 0
-				bp = row[7].replace(',', '')
-				ul = row[8].replace(',', '')
-				ll = row[9].replace(',', '')
-				print '%s,%g,%g,%g' % (row[0], Decimal(bp) *10, Decimal(ul) *10, Decimal(ll) *10)
-			rownum = 1
+        reader = csv.reader(ifile)
+        rownum = 0
+        for row in reader:
+            # Save header row.
+            if rownum == 0:
+                header = row
+            else:
+                colnum = 0
+                bp = row[7].replace(',', '')
+                ul = row[8].replace(',', '')
+                ll = row[9].replace(',', '')
+                print('%s,%g,%g,%g' % (row[0], Decimal(bp) *10, Decimal(ul) *10, Decimal(ll) *10))
+            rownum = 1
 
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "f:h")
     except getopt.GetoptError as e:
-        print e
+        print (e)
         sys.exit()
 
     try:
@@ -47,9 +60,10 @@ def main():
                 printHelp()
                 sys.exit(0)
 
-        process_file(DATA_FILE)
+        if (DATA_FILE):
+            process_file(DATA_FILE)
 
-    except Exception, err:
+    except Exception as err:
         traceback.print_exc(file=sys.stderr)
         sys.stderr.flush()
         print("Error: %s\n" %str(err))
